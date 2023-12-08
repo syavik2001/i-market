3faa87374ae7f8ffcc2719b9ad7cb2c55797698a
Vyacheslav Vinichenko <syavik2001@gmail.com>
2023-12-09 01:42:18 +0200

    Updates

---
+++ static/js/NotFound.5c6e2f2a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunki_market=self.webpackChunki_market||[]).push([[187],{9651:function(n,e,r){r.r(e),r.d(e,{default:function(){return s}});r(2791);var t="NotFoundBlock_root__FxcIK",u=r(184),c=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)("h1",{className:t,children:"\u0421\u0442\u043e\u0440\u0456\u043d\u043a\u0443 \u043d\u0435 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u043e :( "})})},s=function(){return(0,u.jsx)(c,{})}}}]);
+//# sourceMappingURL=NotFound.5c6e2f2a.chunk.js.map
