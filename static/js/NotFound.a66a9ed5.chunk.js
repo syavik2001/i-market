1be16d33d07f5b49bd8e04e7850c1c265d5041a3
Vyacheslav Vinichenko <syavik2001@gmail.com>
2024-01-19 13:11:05 +0200

    Updates

---
+++ static/js/NotFound.a66a9ed5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunki_market=self.webpackChunki_market||[]).push([[187],{9651:function(n,t,e){e.r(t),e.d(t,{default:function(){return o}});e(2791);var r=e(9230),u="NotFoundBlock_root__FxcIK",c=e(184),a=function(){var n=(0,r.$G)().t;return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("h1",{className:u,children:[n("Page not found :(")," "]})})},o=function(){return(0,c.jsx)(a,{})}}}]);
+//# sourceMappingURL=NotFound.a66a9ed5.chunk.js.map
